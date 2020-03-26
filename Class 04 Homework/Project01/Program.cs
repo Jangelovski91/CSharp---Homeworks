acc8b0befc584a59e9b28ec89a62d4c4c735a2f5
Madridista1400 <a.jangelovski@yahoo.com>
2020-03-26 17:54:03 +0100

    additional commit

---
+++ Class 04 Homework/Project01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Project01
+{
+    class Program
+    {   
+
+        static void Main(string[] args)
+        {
+
+
+           Substrings();
+            Console.ReadLine();
+
+        }
+        static void Substrings()
+        {
+
+            string hello = "Hello from SEDC Codecademy v7.0";
+            Console.WriteLine(hello);
+            Console.WriteLine($"Please enter a number n. from 1 to {hello.Length} ");
+            int N = int.Parse(Console.ReadLine());
+            if (N < 1 || N > hello.Length)
+            {
+                Console.WriteLine("Wrong number!");
+            }
+            else
+            {
+                string substring1 = hello.Substring(0, N);
+                Console.WriteLine($@"The first {N} characters are ""{substring1}"" and the length of this substring is {substring1.Length}...");
+            }
+
+
+}
+        
+    }
+}
