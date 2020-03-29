52d969f48db3fc37ff866172d8d3f3f7972b82ef
Madridista1400 <a.jangelovski@yahoo.com>
2020-03-29 19:19:15 +0200

    additional commit

---
+++ Class 05 Homework/Project01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime add3Days = DateTime.Today.AddDays(3);
+            Console.WriteLine(add3Days);
+            DateTime addOneMonth = DateTime.Today.AddMonths(1);
+            Console.WriteLine(addOneMonth);
+            DateTime add1M3D = DateTime.Today.AddMonths(1).AddDays(3);
+            Console.WriteLine(add1M3D);
+            DateTime Oney2mAgo = DateTime.Today.AddYears(-1).AddMonths(-2);
+            Console.WriteLine(Oney2mAgo);
+            DateTime currentMonth = DateTime.Now;
+            Console.WriteLine( currentMonth.ToString("MMMM"));
+            DateTime currentYear = DateTime.Now;
+            Console.WriteLine(currentYear.ToString("yyyy"));
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class 05 Homework/Project02-Dog/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Project02_Dog
+{   
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Color { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine($"The dog {Name} is now eating.");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine($"The dog {Name} is now playing!");
+        }
+
+        public void ChaseTail()
+        {
+            Console.WriteLine("The dog now is chasing its tail! :D");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Max = new Dog();
+            Console.WriteLine("Enter dog's name.");
+            Max.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter dog's race.");
+            Max.Race = Console.ReadLine();
+
+            Console.WriteLine("Enter dog's color.");
+            Max.Color = Console.ReadLine();
+            Console.WriteLine("Choose one of next dog's actions:Eat, Play, ChaseTail...");
+            string DogsAction = Console.ReadLine();
+            if(DogsAction == "Eat")
+            {
+                Max.Eat();
+            } else if(DogsAction == "Play")
+            {
+                Max.Play();
+            } else if(DogsAction == "ChaseTail")
+            {
+                Max.ChaseTail();
+            } else
+            {
+                Console.WriteLine("Wrong Input!"); 
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
