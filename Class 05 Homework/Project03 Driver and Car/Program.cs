5d2e9421988d13289d4068e842ed239c09064867
Dejan <djovanov92@gmail.com>
2020-04-01 18:23:04 +0200

    comments added in Driver excercise

--- Class 05 Homework/Project03 Driver and Car/Program.cs
+++ Class 05 Homework/Project03 Driver and Car/Program.cs
@@ -91,15 +91,25 @@ namespace Project03_Driver_and_Car
 
             Console.WriteLine(Porshe.CalculateSpeed(Bob));
             Console.WriteLine(RaceCars(Greg, Porshe));
+            // tuka treba da imas vo porakata primer vnesi broj od 1 - 4 za da izberes vozac
+            // mozes ako sakas i da gi ispecatis vozacite za da znae koj e pod koj broj
             Console.WriteLine("Please select your first driver.");
+
+            // ova bi se vikalo nesto kako userChoice ili chosenNumber
             string firstDriver = Console.ReadLine();
+
+            // ova treba da ti bide Driver firstDriver = null;
             string driver = null;
             switch (driver)
             {
                 case "1":
+                    // ajde probaj tuka namesto string da stavas vo varijablata da go stavis celiot objekt
+                    // znaci treba vo varijablata firstDriver sto ke ti bide inicijalno null (linija 101)
+                    // da go stavis objektot vo koj go cuvas vozacot 1 (a toa e bob)
                     driver = "Bob";
                     break;
                 case "2":
+                    // istoto od prviot slucaj vazi sekade
                     driver = "Greg";
                     break;
                 case "3":
@@ -111,6 +121,7 @@ namespace Project03_Driver_and_Car
                 default:
                     break;
             }
+            // ova e vishok
             firstDriver = driver;
             
            
