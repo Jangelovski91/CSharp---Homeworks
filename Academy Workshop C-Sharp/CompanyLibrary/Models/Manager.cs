8d9b7fcc39687b5e8c4e61ab93e22a3d03264f06
Madridista1400 <a.jangelovski@yahoo.com>
2020-04-11 14:09:19 +0200

    additional commit

---
+++ Academy Workshop C-Sharp/CompanyLibrary/Models/Manager.cs
@@ -0,0 +1,32 @@
+﻿using CompanyLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLibrary.Models
+{
+    public class Manager : Employee
+    {
+        private double Bonus { get; set; }
+        public Manager()
+        {
+
+        }
+        public Manager(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = 5000;
+            Role = RoleType.Manager;
+        }
+        public void AddBonus(double bonus)
+        {
+            Bonus = bonus;
+        }
+        public override double GetSalary()
+        {
+            return Salary += Bonus;
+        }
+    }
+
+}
