d7d50c1abd14d9f690f5d694ab3e5d87baf27089
Madridista1400 <a.jangelovski@yahoo.com>
2020-05-24 20:31:01 +0200

    Linq Homework SpecialEdition

---
+++ Class 10 LINQ.SpecialEdition.Homework/Linq.Homework/Models/ArtistType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq.Homework.Models
+{
+    public enum ArtistType
+    {
+        Band,
+        SoloArtist
+    }
+}
